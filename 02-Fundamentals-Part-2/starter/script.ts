df4881e93d1295d53ae18c07bf985a52ef5db524
Richard Au <au.richard4@gmail.com>
2022-07-25 15:14:16 -0700

    Changing code to Typescript.

---
+++ 02-Fundamentals-Part-2/starter/script.ts
@@ -0,0 +1,32 @@
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("I can drive :D");
+
+//Coding Challenge #1: Functions and Arrow Functions
+const calcAverage = (score1: number, score2: number, score3: number) => {
+  return (score1 + score2 + score3) / 3;
+};
+
+//Test 1
+const scoreDolphins = calcAverage(44, 23, 71);
+const scoreKoalas = calcAverage(65, 54, 49);
+console.log(scoreDolphins, scoreKoalas);
+
+
+const checkWinner = (avgDolphins: number, avgKoalas: number) => {
+  if (avgDolphins >= 2 * avgKoalas) {
+    console.log(`Dolphins win! (${avgDolphins} vs. ${avgKoalas}).`);
+  } else if (avgKoalas >= 2 * avgDolphins) {
+    console.log(`Koalas win! (${avgKoalas} vs. ${avgDolphins}).`);
+  } else {
+    console.log("No team wins.");
+
+  }
+};
+
+
+
+calcAverage(44, 23, 71);
+calcAverage(65, 74, 49);
