958a399fbb6c97b0eaf563623fb5282cb06a08ca
Richard Au <au.richard4@gmail.com>
2022-07-25 15:15:04 -0700

    Doing Test 1 in challenge 1.

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,24 +1,28 @@
 "use strict";
-
 let hasDriversLicense = false;
 const passTest = true;
-
-if (passTest) hasDriversLicense = true;
-if (hasDriversLicense) console.log("I can drive :D");
-
+if (passTest)
+    hasDriversLicense = true;
+if (hasDriversLicense)
+    console.log("I can drive :D");
 //Coding Challenge #1: Functions and Arrow Functions
-const finalScores = [];
 const calcAverage = (score1, score2, score3) => {
-  const avgScore = (score1 + score2 + score3) / 3;
-  finalScores.push(avgScore);
-  return finalScores;
+    return (score1 + score2 + score3) / 3;
 };
-
+//Test 1
+const scoreDolphins = calcAverage(44, 23, 71);
+const scoreKoalas = calcAverage(65, 54, 49);
+console.log(scoreDolphins, scoreKoalas);
 const checkWinner = (avgDolphins, avgKoalas) => {
-  if (avgDolphins >= 2 * avgKoalas) {
-    console.log(`Dolphins win! (${avgDolphins} vs. ${avgKoalas}).`);
-  }
+    if (avgDolphins >= 2 * avgKoalas) {
+        console.log(`Dolphins win! (${avgDolphins} vs. ${avgKoalas}).`);
+    }
+    else if (avgKoalas >= 2 * avgDolphins) {
+        console.log(`Koalas win! (${avgKoalas} vs. ${avgDolphins}).`);
+    }
+    else {
+        console.log("No team wins.");
+    }
 };
-
 calcAverage(44, 23, 71);
 calcAverage(65, 74, 49);
